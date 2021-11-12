bec567f31921a77be91353a807c79ca0bf6fde18
Zakhar <39008563+ZakharTS@users.noreply.github.com>
2021-11-12 19:50:46 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,3 @@
+create database db;
+use db;
 create table `files` (`id` INT NOT NULL AUTO_INCREMENT, `datetime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `filename` CHAR(50) NOT NULL, PRIMARY KEY(`id`));
